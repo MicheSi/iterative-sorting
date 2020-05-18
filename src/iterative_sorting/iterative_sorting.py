36171cb1edc98a4dad25fd814e00a86ad75371ba
mlazzell17@gmail.com <MicheSi>
2020-05-18 14:38:09 -0700

    working on count sort for stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -34,6 +34,24 @@ def bubble_sort(arr):
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
-
-
+    # find max element in array
+    length = len(arr)
+    temp = [0] * length
+    # initialize length + 1 with all elements 0 - used for storing count
+    count = [0] * temp
+    # store count of each element in count array
+    for i in range(0, length):
+        count[arr[i]] += 1
+    # store cumulative sum of elements in count array
+    for i in range(1, maximum):
+        count[i] += count[i - 1]
+    # find index of each element of original array in count array
+    # decrease by 1
+    i = length - 1
+    while i >= 0:
+        temp[count[arr[i]] - 1] = arr[i]
+        count[arr[i]] -= 1
+    # copy sorted elements into original array
+    for i in range(0, length):
+        arr[i] = temp[i]
     return arr
