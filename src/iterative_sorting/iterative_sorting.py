72f1baed113d55e61ebcb962e4408a49286e675d
mlazzell17@gmail.com <MicheSi>
2020-05-19 11:58:27 -0700

    count sort completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -32,26 +32,48 @@ def bubble_sort(arr):
 
 
 # STRETCH: implement the Count Sort function below
+# def count_sort(arr, maximum=-1):
+#     # Your code here
+#     # find max element in array
+#     length = len(arr)
+#     temp = [0] * length
+#     # initialize length + 1 with all elements 0 - used for storing count
+#     count = [0] * temp
+#     # store count of each element in count array
+#     for i in range(0, length):
+#         count[arr[i]] += 1
+#     # store cumulative sum of elements in count array
+#     for i in range(1, maximum):
+#         count[i] += count[i - 1]
+#     # find index of each element of original array in count array
+#     # decrease by 1
+#     i = length - 1
+#     while i >= 0:
+#         temp[count[arr[i]] - 1] = arr[i]
+#         count[arr[i]] -= 1
+#     # copy sorted elements into original array
+#     for i in range(0, length):
+#         arr[i] = temp[i]
+#     return arr
+
 def count_sort(arr, maximum=-1):
-    # Your code here
-    # find max element in array
-    length = len(arr)
-    temp = [0] * length
-    # initialize length + 1 with all elements 0 - used for storing count
-    count = [0] * temp
-    # store count of each element in count array
-    for i in range(0, length):
-        count[arr[i]] += 1
-    # store cumulative sum of elements in count array
-    for i in range(1, maximum):
-        count[i] += count[i - 1]
-    # find index of each element of original array in count array
-    # decrease by 1
-    i = length - 1
-    while i >= 0:
-        temp[count[arr[i]] - 1] = arr[i]
-        count[arr[i]] -= 1
-    # copy sorted elements into original array
-    for i in range(0, length):
-        arr[i] = temp[i]
+    if len(arr) == 0:
+        return arr
+
+    if maximum == -1:
+        maximum = max(arr)
+
+    buckets = [0 for _ in range(maximum + 1)]
+
+    for x in arr:
+        if x < 0:
+            return 'Error, negative numbers not allowed in Count Sort'
+        buckets[x] += 1
+
+    j = 0
+    for i in range(len(buckets)):
+        while buckets[i] > 0:
+            arr[j] = i
+            j += 1
+            buckets[i] -= 1
     return arr
